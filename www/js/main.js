e6ecac5f69a4beff33bd37f9786d7f07af2770e3
dragonqfly <dragonqfly@gmail.com>
2016-12-31 01:56:11 -0600

    first commit

---
+++ www/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){  
+
+	$("#flogin").submit(function( event ) {
+		event.preventDefault();
+		var dataform1 = $(this).serialize(); //serializearray
+		//contentType: "application/json; charset=utf-8",
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: "http://cmcoem.info/webservice.php",
+			data: dataform1,
+			success: function(result) {
+				//window.alert('Entro');
+				//console.log(result);
+				if(result){
+					if(result.status.status == 'susses'){
+						$(".navbar-header button").click();
+						var listas = '';
+						$.each(result.lista_socios, function( key, value ){
+							listas += '<tr><td>'+ value.nombre +'</td><td>'+ value.tipo_socio +'</td></tr>';
+						});
+						$("#contenido").html( '<h4>Lista de Socios</h4><table border="1"><tr><td>Nombre</td><td>Tipo de socio</td></tr>'+ listas +'</table>' );
+						window.alert('Se ha cargado la lista exitosamente');
+					}else{
+						window.alert(result.status.message);
+					}
+				}else{
+					window.alert('Error');
+				}
+			},
+			error: function(msg) {
+				window.alert("Error de conexion");
+				console.log(msg);
+			}
+		});
+
+	}); 
+}); 
+
